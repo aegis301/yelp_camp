ae4fd9e5672de88649600496aaeedb743ecf21ca
Christian <christian.porschen@gmail.com>
2021-01-18 16:56:10 +0100

    complete img delte feature and thumbnail virtual

--- models/campground.js
+++ models/campground.js
@@ -1,40 +1,45 @@
-const mongoose = require('mongoose') // require mongoose
+const mongoose = require("mongoose"); // require mongoose
+const { estimatedDocumentCount } = require( "./review" );
 const Schema = mongoose.Schema; // save Schema expression to shorten ref
-const Review = require('./review')
+const Review = require("./review");
 
-// define Campground Schema
-const CampgroundSchema = new Schema ({
-    title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
-    price: Number,
-    description: String,
-    location: String,
-    author: { 
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-     },
-    reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Review'
-        }
-    ]
+// define img schema
+const ImageSchema = new Schema({
+	url: String,
+	filename: String,
 });
 
-CampgroundSchema.post('findOneAndDelete', async function (doc) {
-    if(doc) {
-        await Review.deleteMany({
-            _id: {
-                $in: doc.reviews
-            }
-        })
-    }
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_200');
 })
+// define Campground Schema
+const CampgroundSchema = new Schema({
+	title: String,
+	images: [ImageSchema],
+	price: Number,
+	description: String,
+	location: String,
+	author: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+	},
+	reviews: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "Review",
+		},
+	],
+});
+
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+	if (doc) {
+		await Review.deleteMany({
+			_id: {
+				$in: doc.reviews,
+			},
+		});
+	}
+});
 
 // convert Schema to model and export it when required
-module.exports = mongoose.model('Campground', CampgroundSchema);
+module.exports = mongoose.model("Campground", CampgroundSchema);
