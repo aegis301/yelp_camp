a491bf9ee894275b3be3082ed965ac7f532e3c1e
Christian <christian.porschen@gmail.com>
2021-01-20 20:52:02 +0100

    basic security

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
 }
-
 
 const port = 3000; //define port
 const express = require("express"); // require express
@@ -25,6 +24,8 @@ const { validationCampgroundSchema, reviewSchema } = require("./schemas");
 const userRoutes = require("./routes/users");
 const reviewRoutes = require("./routes/reviews");
 const campgroundRoutes = require("./routes/campground");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
 	useNewUrlParser: true,
@@ -50,14 +51,62 @@ app.set("views", path.join(__dirname, "views")); // use path module to tell expr
 app.use(express.urlencoded({ extended: true })); // tells express to parse the body in a post request
 app.use(methodOverride("_method")); // create method-override
 app.use(express.static(path.join(__dirname, "public"))); // serve static directories
+app.use(mongoSanitize());
+app.use(helmet({ contentSecurityPolicy: false }));
+
+const scriptSrcUrls = [
+	"https://stackpath.bootstrapcdn.com/",
+	"https://api.tiles.mapbox.com/",
+	"https://api.mapbox.com/",
+	"https://kit.fontawesome.com/",
+	"https://cdnjs.cloudflare.com/",
+	"https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+	"https://kit-free.fontawesome.com/",
+	"https://stackpath.bootstrapcdn.com/",
+	"https://api.mapbox.com/",
+	"https://api.tiles.mapbox.com/",
+	"https://fonts.googleapis.com/",
+	"https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+	"https://api.mapbox.com/",
+	"https://a.tiles.mapbox.com/",
+	"https://b.tiles.mapbox.com/",
+	"https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: [],
+			connectSrc: ["'self'", ...connectSrcUrls],
+			scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+			styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+			workerSrc: ["'self'", "blob:"],
+			objectSrc: [],
+			imgSrc: [
+				"'self'",
+				"blob:",
+				"data:",
+				"https://res.cloudinary.com/ddiurydgo/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+				"https://images.unsplash.com/",
+			],
+			fontSrc: ["'self'", ...fontSrcUrls],
+		},
+	})
+);
 
 // setup express sessions
 const sessionConfig = {
+	name: "ycsession",
 	secret: "thisshouldbeabettersecret",
 	resave: false,
 	saveUninitialized: true,
 	cookie: {
 		httpOnly: true,
+		// secure: true,
 		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
 		maxAge: 1000 * 60 * 60 * 24 * 7,
 	},
--- schemas.js
+++ schemas.js
@@ -1,20 +1,46 @@
-const Joi = require("joi");
+const BaseJoi = require("joi");
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+	type: "string",
+	base: joi.string(),
+	messages: {
+		"string.escapeHTML": "{{#label}} must not include HTML!",
+	},
+	rules: {
+		escapeHTML: {
+			validate(value, helpers) {
+				const clean = sanitizeHtml(value, {
+					allowedTags: [],
+					allowedAttributes: {},
+				});
+				if (clean !== value)
+					return helpers.error("string.escapeHTML", { value });
+				return clean;
+			},
+		},
+	},
+});
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.validationCampgroundSchema = Joi.object({
 	campground: Joi.object({
-		title: Joi.string().required(),
+		title: Joi.string().required().escapeHTML(),
 		price: Joi.number().required().min(0),
-		description: Joi.string().required(),
-		location: Joi.string().required(),
+		description: Joi.string().required().escapeHTML,
+		location: Joi.string().required().escapeHTML(),
 	}).required(),
-	image: Joi.alternatives().try(Joi.array(), Joi.string()).allow(null).allow(''),
-	deleteImages: Joi.array()
+	image: Joi.alternatives()
+		.try(Joi.array(), Joi.string())
+		.allow(null)
+		.allow(""),
+	deleteImages: Joi.array(),
 });
-
 
 module.exports.reviewSchema = Joi.object({
 	review: Joi.object({
-			rating: Joi.number().required().min(1).max(5),
-			reviewbody: Joi.string().required()
-	}).required()
-})
+		rating: Joi.number().required().min(1).max(5),
+		reviewbody: Joi.string().required().escapeHTML(),
+	}).required(),
+});
